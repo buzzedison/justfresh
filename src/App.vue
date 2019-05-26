8bea4ae48d62108ffa215b4066429236a6add210
Edison <buzzedison@gmail.com>
2019-05-26 01:12:38 +0000

    first commit from vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+   
+    <Main/>
+    <Intro/>
+  </div>
+</template>
+
+<script>
+import Main from './components/main.vue'
+import Intro from './components/intro.vue'
+export default {
+  name: 'app',
+  components: {
+    Main,
+    Intro
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,50 @@
+<template>
+ <div>
+   
+     <v-toolbar color="orange">
+   
+    <v-toolbar-title>Just Fresh</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Home</v-btn>
+      <v-btn flat>About</v-btn>
+      <v-btn flat>Food</v-btn>
+      <v-btn flat>Insight</v-btn>
+      <v-btn flat>Juices</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.v-btn {
+  color:white;
+  font-family: roboto;
+  font-size: 18px;
+}
+</style>
