92c43d43f1c907fe9cc81a4589230cdf64b76184
Edison <buzzedison@gmail.com>
2019-05-26 02:09:34 +0000

    home page edit

--- src/components/body.vue
+++ src/components/body.vue
@@ -37,11 +37,10 @@
         >
           <v-icon>mdi-cart</v-icon>
         </v-btn>
-        <div class="font-weight-light grey--text title mb-2">For the perfect meal</div>
-        <h3 class="display-1 font-weight-light orange--text mb-2">QW cooking utensils</h3>
+        <div class="font-weight-light grey--text title mb-2">Check out our food options</div>
+        <h3 class="display-1 font-weight-light orange--text mb-2">Breakfast & Lunch</h3>
         <div class="font-weight-light title mb-2">
-          Our Vintage kitchen utensils delight any chef.<br>
-          Made of bamboo by hand
+          Are you hungry? Click to order food.
         </div>
       </v-card-text>
     </v-card>
@@ -66,7 +65,7 @@
             class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
             style="height: 100%;"
           >
-            $14.99
+            from GHC 20
           </div>
         </v-expand-transition>
       </v-img>
@@ -85,11 +84,10 @@
         >
           <v-icon>mdi-cart</v-icon>
         </v-btn>
-        <div class="font-weight-light grey--text title mb-2">For the perfect meal</div>
-        <h3 class="display-1 font-weight-light orange--text mb-2">QW cooking utensils</h3>
+        <div class="font-weight-light grey--text title mb-2">Get healthy Juices and Smoothies</div>
+        <h3 class="display-1 font-weight-light orange--text mb-2">Best juices and smoothies</h3>
         <div class="font-weight-light title mb-2">
-          Our Vintage kitchen utensils delight any chef.<br>
-          Made of bamboo by hand
+          Everyhing is fresh
         </div>
       </v-card-text>
     </v-card>
